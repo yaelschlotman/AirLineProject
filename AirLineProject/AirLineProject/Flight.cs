56de4496a53a2c899a630c1fd03d37e0bc53fc40
berdo272 <berdo272@yahoo.com>
2016-02-01 14:01:37 -0600

    Initial Comit, Classes and member variables laid out

---
+++ AirLineProject/AirLineProject/Flight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirLineProject
+{
+    class Flight
+    {
+        Aircraft Aircraft;
+        int FlightNumber;
+        string Origin;
+        string Destination;
+        List<Passenger> PassengerManifest;
+
+    }
+}
---
+++ AirLineProject/AirLineProject/Passenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirLineProject
+{
+    class Passenger
+    {
+        string Name;
+        string SeatAssignment;
+        Flight Flight;
+    }
+}
