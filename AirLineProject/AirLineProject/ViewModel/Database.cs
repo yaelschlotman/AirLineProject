d4c4373026bb97f4ee080fa478d70239716d672a
berdo272 <berdo272@yahoo.com>
2016-02-03 15:12:38 -0600

    Clarified What View Model actually is and move files to correct folder. Cleaned up unused logic in Database.cs

---
+++ AirLineProject/AirLineProject/ViewModel/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirLineProject.Model
+{
+    public class Database
+    {
+        public List<Flight> FlightList;
+        public List<Aircraft> Fleet;
+
+        public Database()
+        {
+            this.FlightList = new List<Flight>();
+            this.Fleet = new List<Aircraft>();
+
+        }
+
+        }
+    }
+}
